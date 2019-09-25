48d61a8034da0f41bff5142ed68c09cff53f89fe
Xwaler <gdgziCXA8939b>
2019-09-25 17:02:07 +0200

    Helloworld

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+class Helloworld {
+	public:
+		static void print() { std::cout << "Hello, World!" << std::endl; }
+};
+
+
+int main() {
+    Helloworld::print();
+    return 0;
+}

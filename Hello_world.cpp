f55efd011765bb094825c27dea5eab261a951d88
Benjamin Caoduro <bcaoduro@pc-gi-101.utbm.local>
2019-09-25 18:50:46 +0200

    Hello-world

---
+++ Hello_world.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+class hello_world{
+
+	public:
+	void affichage()
+	{
+		std::cout<<"Hello World";
+	}
+	
+};
+
+int main()
+{
+	hello_world a;
+	a.affichage();
+
+return 0;
+}
+
+

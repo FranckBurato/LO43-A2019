f1ecc8b068a73e2de9e0823ce4bc716aa43100b3
Hao Ling Zhang <hzhang@pc-gi-107.utbm.local>
2019-09-25 18:47:41 +0200

    helloworld

---
+++ helloworld.cpp
@@ -0,0 +1,21 @@
+#include<iostream>  
+using namespace std;
+
+
+class helloworld
+{
+public:
+void afficher ()
+{
+    cout<<"Hello World"; 
+}
+};
+
+
+int main() 
+{ 
+  helloworld he;
+ he.afficher();
+    return 0; 
+} 
+

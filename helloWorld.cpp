5f15966b383d17ae26f8a6d219c8a8260b2a8e6b
Benjamin Kieffer <bkieff01@pc-gi-109.utbm.local>
2019-09-25 18:20:27 +0200

    première tentative

---
+++ helloWorld.cpp
@@ -0,0 +1,28 @@
+#include "helloWorld.h"
+using namespace std;
+
+	/*
+
+	HelloWorld::HelloWorld ()
+	{		
+
+	}
+
+	HelloWorld::HelloWorld (const HelloWorld& other)
+	{
+
+	}
+
+	virtual HelloWorld::~HelloWorld(){};
+
+	HelloWorld::HelloWorld& operator=(const Helloworld& other)
+	{
+
+	}*/
+
+	void HelloWorld::AfficherHelloWorld (void)
+	{
+		cout << "HelloWorld !";
+
+		return;
+	}
---
+++ helloWorld.h
@@ -0,0 +1,19 @@
+/*#ifndef HELLOWORLD_H
+#define HELLOWORLD_H
+*/
+#include <iostream>
+using namespace std;
+
+class HelloWorld
+{
+	public : 
+
+	/*HelloWorld ();
+	HelloWorld (const HelloWorld& other);
+	virtual ~HelloWorld();
+	HelloWorld& operator=(const HelloWorld& other);*/
+	void AfficherHelloWorld (void);
+
+};
+
+//#endif

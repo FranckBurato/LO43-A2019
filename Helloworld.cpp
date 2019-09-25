13b17d81c6aa1df8786ba2c827b5c23157119b4c
WarkM <luka.lambalot@hotmail.fr>
2019-09-25 16:50:58 +0200

    First commit : HelloWorld.c .h

---
+++ Helloworld.cpp
@@ -0,0 +1,13 @@
+#include "Helloworld.h"
+
+void Helloworld::affiche(){
+    cout << "Hello World! ;) ";
+};
+
+
+int main() 
+{
+    Helloworld hello;
+    hello.affiche();
+    return 0;
+}
---
+++ Helloworld.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+class Helloworld{
+    public:
+        void affiche();
+        
+};

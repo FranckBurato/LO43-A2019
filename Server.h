adc762e2bf00a956d20416b5f6cf65a0bf833a24
RENYihua-736 <55799793+RENYihua-736@users.noreply.github.com>
2019-10-06 13:07:00 +0200

    Add files via upload

---
+++ Server.h
@@ -0,0 +1,27 @@
+//
+//  server.h
+//  lo43tp
+//
+//  Created by 任怡桦 on 2019/10/5.
+//  Copyright © 2019年 renyihua. All rights reserved.
+//
+
+#ifndef Server_h
+#define Server_h
+#include<iostream>
+using namespace std;
+
+class Server{
+private:
+    int capteur;
+public:
+    Server(int capteur);//constructeur par défaut
+    ~Server();//destructeur
+    Server(const Server& s);//constructeur par recopie
+    Server& operator=(const Server& s);//operator d'affectation
+    void consoleWrite();//visualiser les données arrivantes dans la console
+    void fileWrite();//stocker les données des capteurs dans des fichiers de logs
+    friend istream& operator>>(istream &input, Server& s);
+};
+
+#endif /* Server_h */

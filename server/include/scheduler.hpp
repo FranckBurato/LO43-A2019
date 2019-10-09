7db2b3e8ba433f8b4ab38a3fda90158a35b70508
pinembour <pinembour@teppelin.fr>
2019-10-09 14:49:29 +0200

    Prepare for TP2 : Create empty scheduler.[cpp|hpp] files for later use in TP2

---
+++ server/include/scheduler.hpp
@@ -0,0 +1,22 @@
+/**
+ * @author      : pinembour (pinembour@$HOSTNAME)
+ * @file        : Scheduler
+ * @created     : Wednesday Oct 09, 2019 14:22:03 CEST
+ */
+
+#ifndef Scheduler_HPP
+
+#define Scheduler_HPP
+
+
+class Scheduler
+{
+    public:
+        Scheduler ();
+        virtual ~Scheduler ();
+    private:
+        /* private data */
+};
+
+#endif /* end of include guard Scheduler_HPP */
+
---
+++ server/src/scheduler.cpp
@@ -0,0 +1,17 @@
+/**
+ * @author      : pinembour (pinembour@$HOSTNAME)
+ * @file        : Scheduler
+ * @created     : Wednesday Oct 09, 2019 14:21:51 CEST
+ */
+
+#include "Scheduler.hpp"
+
+Scheduler::Scheduler()
+{
+    
+}
+
+Scheduler::~Scheduler()
+{
+}
+

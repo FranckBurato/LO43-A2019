7e14d1e392ed01fb6f2b6eac4c4bd6a50561d056
EdgarDupont <55579204+EdgarDupont@users.noreply.github.com>
2019-10-09 15:58:21 +0200

    Update Temperature.h

--- Temperature.h
+++ Temperature.h
@@ -3,5 +3,14 @@ class Temperature public Sensor
 private:
 	float temperature;
 public:
-	
+	Temperature();
+	Temperature(const Temperature& autre)
+	{
+		
+	};
+	~Temperature();
+	Temperature& operator=(const Temperature& autre)
+	{
+		
+	};
 }

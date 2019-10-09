768bf0d440508f2304c626e3e027b2a957d4991b
BenjaminKieffer-01 <55802186+BenjaminKieffer-01@users.noreply.github.com>
2019-10-09 19:03:49 +0200

    Add files via upload
    commit 3

--- server.cpp
+++ server.cpp
@@ -1,13 +1,22 @@
-#include "server.h"
+include "server.h"
 
-server::server() 
+server::server():nbrOfSensors(0),consolActivation(0),logActivation(0)
 {
-	//Constructeur
+
 }
 
 server::server(const server& other)
 {
-	//constructeur par recopie
+	nbrOfSensors=other.nbrOfSensors;
+	consolActivation=other.consolActivation;
+	logActivation=other.logActivation;
+}
+
+server::server(int a, bool b, bool c)
+{
+	nbrOfSensors=a;
+	consolActivation=b;
+	logActivation=c;
 }
 
 server::~server()
@@ -17,31 +26,93 @@ server::~server()
 
 server& server::operator=(const server& other)
 {
-
+    nbrOfSensors=other.nbrOfSensors;
+	consolActivation=other.consolActivation;
+	logActivation=other.logActivation;
+	return *this;
 }
 
-void server::operator<<(const server& other,float donnée)
+/*void operator<<(string dataSensorToString, int sensorLog)
 {
-	consoleWrite(d);
-	fileWrite(d);
-}
+    fileWrite(dataSensorToString,sensorLog);
+    return;
+}*/
 
-void consoleWrite (float d)
+void server::operator<<(int dataSensor)
 {
-	cout << "Valeur de la donnée arrivante :" << d << " .";
+    consoleWrite(dataSensor);
+    return;
 }
 
-void fileWrite (float d)
-{	
-	ofstream fluxLog1("home/users/bkieff01/Documents/LO43TP/TP1/TP1 SUITE/log1.txt");
-	
-	if (fluxLog1)
-	{
-		fluxLog1 << d << "," << endl;
-	}
-	else
-	{	
-		cout << "erreur : fichier introuvable" << endl;
-	}
+
+
+void consoleWrite (int dataSensor)
+{
+	cout << "Valeur de la donnée arrivante :" << dataSensor << " .";
 	return;
 }
+
+void fileWrite (string dataSensorToString, int sensorLog)
+{
+    switch (sensorLog)
+        {
+
+        case 1:
+        {
+            ofstream fluxLog1("home/users/bkieff01/Documents/LO43TP/TP1/TP1 SUITE/log1.txt");
+            if (fluxLog1)
+            {
+                fluxLog1 << dataSensorToString << "," << endl;
+            }
+            else
+            {
+                cout << "erreur : fichier log 1 introuvable" << endl;
+            }
+        break;
+        }
+
+        case 2:
+        {
+            ofstream fluxLog2("home/users/bkieff01/Documents/LO43TP/TP1/TP1 SUITE/log2.txt");
+            if (fluxLog2)
+            {
+                fluxLog2 << dataSensorToString << "," << endl;
+            }
+            else
+            {
+                cout << "erreur : fichier log 2 introuvable" << endl;
+            }
+        break;
+        }
+
+        case 3:
+        {
+            ofstream fluxLog3("home/users/bkieff01/Documents/LO43TP/TP1/TP1 SUITE/log3.txt");
+            if (fluxLog3)
+            {
+                fluxLog3 << dataSensorToString << "," << endl;
+            }
+            else
+            {
+                cout << "erreur : fichier log 3 introuvable" << endl;
+            }
+        break;
+        }
+        case 4:
+        {
+            ofstream fluxLog4("home/users/bkieff01/Documents/LO43TP/TP1/TP1 SUITE/log4.txt");
+            if (fluxLog4)
+            {
+                fluxLog4 << dataSensorToString << "," << endl;
+            }
+            else
+            {
+                cout << "erreur : fichier log 4 introuvable" << endl;
+            }
+        break;
+        }
+    }
+	return;
+}
+
+
--- server.h
+++ server.h
@@ -1,21 +1,39 @@
+#ifndef SERVER_H
+#define SERVER_H
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
-class server 
+class server
 {
+    private :
+
+        int nbrOfSensors;
+        bool consolActivation;
+        bool logActivation;
+
 	public :
+
 		server ();
 		server (const server& other);
+		server (int,bool,bool);
 		virtual ~server();
-		server& operator=(const server& other); 
-		//surchage doit permettre de copier le contenu d'un objet dans l'objet courant;
+		server& operator=(const server& other);
+		//surchage doit permettre de copier le contenu d'un objet dans l'objet courant
 
-		server& operator<<(const server& other);
-		//surcharge doit : element que l'on souhaite afficher doit etre redirigé vers la console et element que l'on souhaite afficher doit etre redirigé vers un fichier de log
+		void operator<<(int dataSensor);
+		//surcharge doit : element que l'on souhaite afficher doit etre redirigé vers la console
 
+        	friend void operator<<(string dataSensorToString, int sensorLog);
+        //Surcharge doit ecrire la donnée du capteur qui a préalablement été transformée d'int en string, il doit l'écrire dans le bon log correspondant au bon sensor
+        //friend pour pouvoir l'utiliser malgré qu'on l'appelle avec 2 arguments
 
-		void consoleWrite (float d);
-		void fileWrite (float d);
+		void consoleWrite (int dataSensor);
+		void fileWrite (string dataSensorToString, int sensorLog);
+		void dataRcv (int dataSensor);
 
-}
+};
+
+#endif // SERVER_H
+

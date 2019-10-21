dfd986d454b01851bde2a909b001ea120634d83a
EdgarDupont <55579204+EdgarDupont@users.noreply.github.com>
2019-10-21 23:07:19 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    return 0;
+}
+void fileWrite (const int& dataSens_p, int nFile)
+{
+	  
+}
+void consolWrite(const int& dataSens_p)
+{
+		std::cout << *dataSens_p << std::endl;
+}

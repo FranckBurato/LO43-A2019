b99403c57d62c30ad95297f47f5dcb6af1fa0c7c
fwitz <francescowitz68@gmail.com>
2019-10-22 21:44:44 +0200

    Corrected bugs related to the link editor with templates

--- src/Sensor.h
+++ src/Sensor.h
@@ -4,12 +4,20 @@
 template<typename T>
 class Sensor{
 	protected:
+		T data;
 		std::string sensorName;
-		std::string formatedData;
 	public:
-		virtual std::string sendData();
-		virtual T getData();
-		virtual void setData(T data);
-		virtual std::string getSensorName() const;
+		virtual std::string sendData(){
+			return "--" + this->getSensorName() + " Sensor-- Value : " + std::to_string(this->getData());
+		}
+		virtual T getData() const{
+			return this->data;
+		}
+		virtual void setData(T data){
+			this->data = data;
+		}
+		virtual std::string getSensorName() const{
+			return this->sensorName;
+		}
 };
 #endif

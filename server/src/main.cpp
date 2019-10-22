1f84355be1392258e41dc9a34ab419c2e85a79c0
pinembour <pinembour@teppelin.fr>
2019-10-22 18:07:58 +0200

    Dumb main that test that everything is working as according to keikaku

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,10 +1,21 @@
-#include "server.hpp"
-#include <iostream>
-#include <string>
+#include "scheduler.hpp"
+#include <ctime>
+#include <cstdlib>
+#include <unistd.h>
 
-#include <fstream>
 
 int main(){
-
+    srand(time(0));
+    Scheduler schedul;
+    while(1){
+        schedul.dataRcv(schedul.tempS);
+        usleep(1000000);
+        schedul.dataRcv(schedul.brightS);
+        usleep(1000000);
+        schedul.dataRcv(schedul.loudS);
+        usleep(1000000);
+        schedul.dataRcv(schedul.humiS);
+        usleep(1000000);
+    }
 	return 0;
 }

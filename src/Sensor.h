91449e7369bdc1abae11fa1204ed071fef117b86
fwitz <francescowitz68@gmail.com>
2019-10-22 21:04:04 +0200

    Transformed the class in abstract class only, not pure abstract class

--- src/Sensor.h
+++ src/Sensor.h
@@ -3,13 +3,13 @@
 
 template<typename T>
 class Sensor{
-	private:
+	protected:
 		std::string sensorName;
 		std::string formatedData;
 	public:
-		virtual std::string sendData() = 0;
-		virtual T getData() = 0;
-		virtual void setData(T data) = 0;
+		virtual std::string sendData();
+		virtual T getData();
+		virtual void setData(T data);
 		virtual std::string getSensorName() const;
 };
 #endif

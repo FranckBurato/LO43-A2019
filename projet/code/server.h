9fad7f1fe4f1d7b4ea4944ad320a04d61f184ec1
taozhehan <55794238+taozhehan@users.noreply.github.com>
2019-10-22 17:26:20 +0200

    Update server.h

--- projet/code/server.h
+++ projet/code/server.h
@@ -22,9 +22,9 @@ public:
 	Server(int nbrOfSensors_p, bool consoleA, bool logA) :nbrOfSensors(nbrOfSensors_p), consolActivation(consoleA), logActivation(logA) {};
 	friend ostream & operator<<(int d,ostream& os); //pour afficher la valeur du int 
 	friend ostream& operator<<(string s ,ostream& os);// pour afficher la valeur du string
-	void consoleWrite(string dataSens_p);//visualiser les donn¨¦e
+	void consoleWrite(string dataSens_p);//visualiser les donnÃ©e
 
-	void fileWrite(string dataSens_p,int i);//stocker les donn¨¦es des capteurs(chaque capteur disposer de son ficher
+	void fileWrite(string dataSens_p,int i);//stocker les donnÃ©es des capteurs(chaque capteur disposer de son ficher
 
 	void dataRcv(string dataSens,int i);//consoleWrite+fileWrite
 };

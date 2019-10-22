a5208ec57cea57dd709ddde5481a6400dfcf7222
fwitz <francescowitz68@gmail.com>
2019-10-22 17:31:53 +0200

    Corrected bugs and syntax

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -4,13 +4,36 @@
 using namespace std;
 
 Scheduler::Scheduler(){
-	this->server = new Server();
+	Server server;
+	this->server = server;
 	this->sensors.resize(0);
+}
+
+Scheduler::Scheduler(int nbrOfSensors, bool consoleActivation, bool logActivation){
+	Server server(nbrOfSensors, consoleActivation, logActivation);
+	Humidity humid;
+	Light light;
+	Sound sound;
+	Temp temp;
+	this->server = server;
+	this->sensors.resize(nbrOfSensors);
+	this->sensors.push_back(humid);
+	this->sensors.push_back(light);
+	this->sensors.push_back(sound);
+	this->sensors.push_back(temp);
 }
 
 Scheduler::Scheduler(Server server){
 	this->server = server;
+	Humidity humid;
+	Light light;
+	Sound sound;
+	Temp temp;
 	this->sensors.resize(server.getNbrOfSensors());
+	this->sensors.push_back(temp);
+	this->sensors.push_back(light);
+	this->sensors.push_back(sound);
+	this->sensors.push_back(temp);
 }
 
 Scheduler::Scheduler(const Scheduler& scheduler){
@@ -26,9 +49,11 @@ Scheduler& Scheduler::operator=(const Scheduler& scheduler){
 	return *this;
 }
 
-void Scheduler::run(){}
+void Scheduler::run(){
 
-Server Scheduler::getServer(){
+}
+
+Server Scheduler::getServer() const{
 	return this->server;
 }
 
@@ -36,10 +61,10 @@ void Scheduler::setServer(Server server){
 	this->server = server;
 }
 
-vector<Sensor> Scheduler::getSensors(){
+vector<variant<Temp, Sound, Light, Humidity>> Scheduler::getSensors() const{
 	return this->sensors;
 }
 
-void Scheduler::setSensors(vector<Sensor> sensors){
+void Scheduler::setSensors(vector<variant<Temp, Sound, Light, Humidity>> sensors){
 	this->sensors = sensors;
 }

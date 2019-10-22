e47110a2d212f9dfe726bcf0cff33a3a8fea6647
fwitz <francescowitz68@gmail.com>
2019-10-22 17:32:29 +0200

    Corrected bugs and syntax

--- src/Scheduler.h
+++ src/Scheduler.h
@@ -1,23 +1,30 @@
 #ifndef DEF_SCHEDULER
 #define DEF_SCHEDULER
 
-#include <vector.h>
 #include "Server.h"
+#include "Sensor.h"
+#include "Light.h"
+#include "Temp.h"
+#include "Humidity.h"
+#include "Sound.h"
+#include <vector>
+#include <variant>
 
 class Scheduler{
 	private:
 		Server server;
-		vector<Sensor> sensors;
+		std::vector<std::variant<Temp, Sound, Light, Humidity>> sensors;
 	public:
 		Scheduler();
+		Scheduler(int nbOfSensors, bool consoleActivation, bool logActivation);
 		Scheduler(Server server);
 		Scheduler(const Scheduler& scheduler);
 		~Scheduler();
 		Scheduler& operator=(const Scheduler& scheduler);
 		void run();
-		Server getServer();
+		Server getServer() const;
 		void setServer(Server server);
-		vector<Sensor> getSensors();
-		void setSensors(vector<Sensor> sensors):
+		std::vector<std::variant<Temp, Sound, Light, Humidity>> getSensors() const;
+		void setSensors(std::vector<std::variant<Temp, Sound, Light, Humidity>> sensors);
 };
 #endif

93248685d26630110141ba6179a1a44fe6356f03
EdgarDupont <55579204+EdgarDupont@users.noreply.github.com>
2019-10-23 13:01:29 +0200

    Update Sensor.h

--- Sensor.h
+++ Sensor.h
@@ -1,11 +1,13 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+//ON UTILISE UN TEMPLATE DATA POUR FLOAT / INT / BOOL
 template <class Data> class Sensor
 {
 private:
-	int dataSens;
-	virtual Data aleaGenVal (void){};
+	Data dataSens;
+//ON AJOUTE VIRTUAL AU METHODES POUR QU'ELLES PUISSENT ETRE REUTILISEES
+	virtual Data aleaGenVal (int nType){};
 public:
 	virtual Sensor()
 	{
@@ -25,9 +27,10 @@ public:
 		this->Sensor.dataSens=autre.dataSens;
 		return *this;
 	};
-	virtual int sendData()
+//METHODE POUR RECUPERER UN DATA ET L'ENVOYER AU SCHEDULER
+	virtual Data sendData()
 	{
-	
+		
 	};
 };
 

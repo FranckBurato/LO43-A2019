9ffff56e8e57f53d943223b471c4f97e435e0791
haolingzhang1 <55579125+haolingzhang1@users.noreply.github.com>
2019-10-23 16:02:26 +0200

    Update Scheduler.h

--- source_code/Scheduler.h
+++ source_code/Scheduler.h
@@ -18,8 +18,6 @@ private:
 	template<class T>
 	friend class Sensor;
 	
-	
-
 public:
 		Scheduler() {}                
 		~Scheduler() {}
@@ -31,43 +29,26 @@ Temperature temperature;
 
 	void inventer(Server& se, Temperature* ter, Humidity* hum, Light* lig, Sound* sou) {
 		se.dataRcv(ter->sendData(), "Temperature : ");
-
 		se.dataRcv(hum->sendData(), "Humidity :");
-
 		se.dataRcv(lig->sendData(), "Light : ");
-
 		se.dataRcv(sou->sendData(), "Sound : ");
+		
 		pid_t pId1 = fork();
-
 		int count1 = 0;
-
 		if (pId1 == -1) {
-
 			perror("fork error");
-
 			exit(EXIT_FAILURE);
-
 		}
 		else if (pId1 == 0) {
-
 			do {
-
 				if (count1 > 4) return EXIT_SUCCESS;
-
 				else {
-
 					s.dataRcv(te.sendData(), te.sendName());
-
 					sleep(1000);
-
 					count1++;
-
 				}
-
 			} while (1);
-
 			return EXIT_SUCCESS;
-
 		}
 
 
@@ -77,32 +58,19 @@ Temperature temperature;
 
 
 		pid_t pId2 = fork();
-
 		if (pId2 == -1) {
-
 			perror("fork error");
-
 			exit(EXIT_FAILURE);
-
 		}
 		else if (pId2 == 0) {
-
 			do {
-
 				if (count1 > 4) return EXIT_SUCCESS;
-
 				else {
-
 					s.dataRcv(hu.sendData(), hu.sendName());
-
 					sleep(2000);
-
 					count1++;
-
 				}
-
 			} while (1);
-
 			return EXIT_SUCCESS;
 
 		}
@@ -116,84 +84,46 @@ Temperature temperature;
 
 
 		pid_t pId3 = fork();
-
 		if (pId3 == -1) {
-
 			perror("fork error");
-
 			exit(EXIT_FAILURE);
-
 		}
 		else if (pId3 == 0) {
-
 			do {
-
 				if (count1 > 4) return EXIT_SUCCESS;
-
 				else {
-
 					s.dataRcv(li.sendData(), li.sendName());
-
 					sleep(3000);
-
 					count1++;
-
 				}
-
 			} while (1);
-
 			return EXIT_SUCCESS;
-
 		}
-
-
-
 
 
 
 
 		pid_t pId4 = fork();
-
 		if (pId4 == -1) {
-
 			perror("fork error");
-
 			exit(EXIT_FAILURE);
-
 		}
 		else if (pId4 == 0) {
-
 			do {
-
 				if (count1 > 4) return EXIT_SUCCESS;
-
 				else {
-
 					s.dataRcv(so.sendData(), so.sendName());
-
 					sleep(4000);
-
 					count1++;
-
 				}
-
 			} while (1);
-
 			return EXIT_SUCCESS;
-
 		}
 
 		wait(&pId1);
-
 		wait(&pId2);
-
 		wait(&pId3);
-
 		wait(&pId4);
-
-
-
-
 
 
 
@@ -201,7 +131,7 @@ Temperature temperature;
 
 
 		
-		//½ÓÊÕ´«¸ĞÆ÷ĞÅÏ¢ÇÒ²»Í¬Ê±¼ä¼ä¸ô´«µİ   
+		//æ¥æ”¶ä¼ æ„Ÿå™¨ä¿¡æ¯ä¸”ä¸åŒæ—¶é—´é—´éš”ä¼ é€’   
 	   }
 
 	

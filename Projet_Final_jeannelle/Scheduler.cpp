21c9cd69a2bf7599aac95ecc2f7a3fd985fa39ad
Emilie Jeannelle <55793894+ejeannelle@users.noreply.github.com>
2019-10-23 09:59:01 +0200

    Update Scheduler.cpp

--- Projet_Final_jeannelle/Scheduler.cpp
+++ Projet_Final_jeannelle/Scheduler.cpp
@@ -17,7 +17,7 @@
 
 
 using namespace std;
-//constructeur par dÈfault
+//constructeur par d√©fault
 Scheduler::Scheduler(){}
 
 Scheduler::Scheduler(int nbS)
@@ -27,7 +27,7 @@ Scheduler::Scheduler(int nbS)
 //destructeur
 Scheduler::~Scheduler(){}
 
-//Surcharge opÈrateur =
+//Surcharge op√©rateur =
 Scheduler& Scheduler::operator=(const Scheduler& nbS)
 {
     this->nbrSec = nbS.nbrSec;
@@ -38,51 +38,52 @@ void Scheduler::sched_run()
 {
 
   Server s;
-  Scheduler she, she2;
   Humidity h;
   Sound so;
   Light li;
   Temperature tm;
+  int sec;
   string touche;
 
   cout<<"Bienvenue dans le programme qui releve les donnees de vos capteurs"<< endl;
   cout<<"Vous avez actuellement 4 capteurs actives : humidite, son, lumiere et temperature "<<endl;
 
-  she.nbrSec=rand()%10 + 10; //DurÈe de la prise de valeurs.
+  this->nbrSec=rand()%10 + 10; //Dur√©e de la prise de valeurs.
+  sec=this->nbrSec;
 
-  cout<<"Les capteurs vous enverront des donnees pendant une duree de " << she.nbrSec<< " secondes."<<endl;
+  cout<<"Les capteurs vous enverront des donnees pendant une duree de " << sec << " secondes."<<endl;
   cout<<"Les donnees vous seront transmises dans des fichiers log mais egalement sur la console"<<endl;
 
   cout<<"\n Entrez ok pour lancer les capteurs"<<endl;
 
 
-  do{ //VÈrifie que l'utilisateur entre bien ok.
+  do{ //V√©rifie que l'utilisateur entre bien ok.
     cin>>touche;
   }while((touche!="ok")&(touche!="OK")&(touche!="Ok"));
 
-   for(auto runUntil = std::chrono::system_clock::now() + std::chrono::seconds(she.nbrSec); std::chrono::system_clock::now() < runUntil;)
-    // on initialise l'heure de fin ‡ l'heure courante + un npmbre aleatoire de secondes
-	// A chaque itÈration on compare l'heure courante ‡ l'heure de fin, si l'heure de fin est depassÈ on sort de la boucle le programme est terminÈe.
+   for(auto runUntil = std::chrono::system_clock::now() + std::chrono::seconds(sec); std::chrono::system_clock::now() < runUntil;)
+    // on initialise l'heure de fin √† l'heure courante + un npmbre aleatoire de secondes
+	// A chaque it√©ration on compare l'heure courante √† l'heure de fin, si l'heure de fin est depass√© on sort de la boucle le programme est termin√©e.
   {
         cout<<"\nCapteur Humidite: "<<endl;//affichage du type de capteur
-        s.dataRvc(h.sendData(h.aleaGenVal(1),1),1);// Envoie des donnÈes du capteur vers le server et affichage des donnÈes.
-        she2.nbrSec=rand()%500 + 200; //GÈnÈration alÈatoire d'un intervalle de temps avant la rÈcupÈration de donnÈe du prochain capteur.
-        Sleep(she2.nbrSec);//arret avant la rÈcupÈration des prochaines donnÈes.
+        s.dataRvc(h.sendData(h.aleaGenVal(1),1),1);// Envoie des donn√©es du capteur vers le server et affichage des donn√©es.
+        this->nbrSec=rand()%500 + 200; //G√©n√©ration al√©atoire d'un intervalle de temps avant la r√©cup√©ration de donn√©e du prochain capteur.
+        Sleep(this->nbrSec);//arret avant la r√©cup√©ration des prochaines donn√©es.
 
         cout<<"\n"<<"Capteur Son: "<<endl;
         s.dataRvc(so.sendData(so.aleaGenVal(2),2),2);
-        she2.nbrSec=rand()%500 + 200;
-        Sleep(she2.nbrSec);
+        this->nbrSec=rand()%500 + 200; //g√©n√©ration d'une valeur entre 200 et 700 secondes
+        Sleep(this->nbrSec);
 
         cout<<"\n"<<"Capteur Lumiere: "<<endl;
         s.dataRvc(li.sendData(li.aleaGenVal(3),3),3);
-        she2.nbrSec=rand()%500 + 200;
-        Sleep(she2.nbrSec);
+        this->nbrSec=rand()%500 + 200;
+        Sleep(this->nbrSec);
 
         cout<<"\n"<<"Capteur Temperature: "<<endl;
         s.dataRvc(tm.sendData(tm.aleaGenVal(4),4),4);
-        she2.nbrSec=rand()%500 + 200;
-        Sleep(she2.nbrSec);
+        this->nbrSec=rand()%500 + 200;
+        Sleep(this->nbrSec);
 
   }
 

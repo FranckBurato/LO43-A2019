c9c5217bcddba5c18415ddb5e6654fd595b9d1b0
fwitz <francescowitz68@gmail.com>
2019-10-23 05:18:33 +0200

    Created run() method. Unable to run it because of an unknown segfault that fill the stack very quickly. Method able to schedule the data getting but unable to get the data due to the recurrent segfault. Communication with the server is fine

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include "Scheduler.h"
 
 using namespace std;
@@ -6,21 +7,18 @@ using namespace std;
 Scheduler::Scheduler(){
 	Server server;
 	this->server = server;
-	this->sensors.resize(0);
 }
 
-Scheduler::Scheduler(int nbrOfSensors, bool consoleActivation, bool logActivation){
-	Server server(nbrOfSensors, consoleActivation, logActivation);
-	Humidity humid;
+Scheduler::Scheduler(bool consoleActivation, bool logActivation){
+	Server server(4, consoleActivation, logActivation);
+	this->server = server;
 	Light light;
 	Sound sound;
 	Temp temp;
-	this->server = server;
-	this->sensors.resize(nbrOfSensors);
-	this->sensors.push_back(humid);
-	this->sensors.push_back(light);
-	this->sensors.push_back(sound);
 	this->sensors.push_back(temp);
+	this->sensors.push_back(sound);
+	this->sensors.push_back(light);
+	this->sensors.push_back(*(new Humidity));	
 }
 
 Scheduler::Scheduler(Server server){
@@ -29,11 +27,10 @@ Scheduler::Scheduler(Server server){
 	Light light;
 	Sound sound;
 	Temp temp;
-	this->sensors.resize(server.getNbrOfSensors());
 	this->sensors.push_back(temp);
-	this->sensors.push_back(light);
 	this->sensors.push_back(sound);
-	this->sensors.push_back(temp);
+	this->sensors.push_back(light);
+	this->sensors.push_back(humid);
 }
 
 Scheduler::Scheduler(const Scheduler& scheduler){
@@ -50,7 +47,39 @@ Scheduler& Scheduler::operator=(const Scheduler& scheduler){
 }
 
 void Scheduler::run(){
+	//vector<int> timers (this->sensors.size, 15);
+	int time = 2; //time in seconds when the scheduler will get the data from the sensors
+	int count = 3; //number of repeat, only for simulation
+	int i=1, j=0;
 
+	chrono::time_point<chrono::steady_clock> end;
+	bool running = true;	
+
+	Humidity humid;
+	cout << humid.getSensorName();
+	
+	end = chrono::steady_clock::now() + chrono::seconds(time);
+	while(running){
+		if(chrono::steady_clock::now() >= end){
+			cout << "Getting data from sensors..." << endl;		
+				cout << "A sensor data" << endl;
+
+			/*for(i=0;i<sensors.size();++i){
+				for(j=0;j<4;++j){
+					server.dataReceive(get<j>(sensors[i]).sendData(),get<j>(sensors[i]).getSensorName);
+				}
+			}*/
+
+			if(i < count){
+				++i;
+				j = 0;
+				end = chrono::steady_clock::now() + chrono::seconds(time);
+			}else{
+				cout << "Ending acquisition" << endl;
+				running = false;
+			}
+		}
+	}
 }
 
 Server Scheduler::getServer() const{
@@ -61,10 +90,10 @@ void Scheduler::setServer(Server server){
 	this->server = server;
 }
 
-vector<variant<Temp, Sound, Light, Humidity>> Scheduler::getSensors() const{
+vector<Sensor> Scheduler::getSensors() const{
 	return this->sensors;
 }
 
-void Scheduler::setSensors(vector<variant<Temp, Sound, Light, Humidity>> sensors){
+void Scheduler::setSensors(vector<Sensor> sensors){
 	this->sensors = sensors;
 }
